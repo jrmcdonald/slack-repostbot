7d84a3256b681d3831f5cc3cc80683edf4a35b37
Jamie McDonald <jamie@qwyck.net>
2016-04-30 15:08:47 +0100

    Moved bot code to separate script in lib.

---
+++ index.js
@@ -0,0 +1,25 @@
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var botkit = require('botkit');
+
+var reconciliation = require('./lib/reconciliation.js');
+var repostbot = require('./lib/repostbot.js');
+
+// Set up the controller uing jfs
+var controller = botkit.slackbot({
+  debug: false,
+  json_file_store: './storage'
+});
+
+// Grab the worker so we can call the Slack Web API directly
+var worker = controller.spawn({
+  token: process.env.token
+}).startRTM()
+
+var bot = new repostbot(controller, worker);
+
+var r = new reconciliation(controller, worker);
+r.reconcileKnownChannels();
